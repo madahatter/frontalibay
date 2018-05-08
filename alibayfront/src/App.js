42a7bf82e5d3f9cbe65e89a44f6757669d30c802
Dickson Lee <dicksont_lee@hotmail.com>
2018-05-08 10:24:51 -0400

    frontest

--- alibayfront/src/App.js
+++ alibayfront/src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Jumbotron,
+  Button,
+  Container,
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Input,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-class App extends Component {
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
+    fetch("/test")
+    .then(e=> e.text())
+    .then(e=>console.log(e))
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Jumbotron>
+         <Container fluid>
+          <h1 className="display-3">Fluid jumbotron</h1>
+          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+         </Container>
+        </Jumbotron>
       </div>
     );
   }
 }
-
-export default App;
