9447c0027de2f6a676c0d20bd3cf5fc14427f256
Dickson Lee <dicksont_lee@hotmail.com>
2018-05-08 16:40:16 -0400

    home, login, reg

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import Home from './Home'
 import Login from './Login';
 import Register from './Register'
 import './App.css';
-import {
-  Jumbotron,
-  Button,
-  Container,
-  Row,
-  Col,
-  NavbarBrand,
-  Navbar
-} from 'reactstrap';
 
 // Render Login Page
 let renderLogin = routerData => {
@@ -25,58 +16,23 @@ let renderRegister = routerData => {
   return (<Register />)
 }
 
+// Render Home Page
+let renderHome = routerData => {
+  return (<Home />)
+}
+
 export default class Example extends React.Component {
   render() {
-    // fetch("/test")
-    // .then(e=> e.text())
-    // .then(e=>console.log(e))
     return (
       <div>
         <BrowserRouter>
-        <div>
-          <Navbar>
-            <ul>
-            <li className="navbar">
-                Alibay
-              </li>
-              <li className="navbar">
-                <input type="text"></input>
-              </li>
-              <li className="navbar">
-                <input type="submit" />
-              </li>
-              <li className="navbar">
-                <Link to={"/login"}> Login </Link>
-              </li>
-              <li className="navbar">
-                <Link to={"/register"}> Register </Link>
-              </li>
-            </ul>
-          </Navbar>
           <div>
-            <Row>
-              <Col xs={6} md={4}>
-                <h4>Categories</h4>
-                <ul>
-                  <li>a</li>
-                  <li>b</li>
-                  <li>c</li>
-                  <li>d</li>
-                </ul>
-              </Col>
-              <Col xs={12} md={8}>
-                <h1>Jumbotron</h1>
-                <p>Loren Ipsum</p>
-              </Col>
-            </Row>
-            <div>
-              <Route exact path='/login' render={renderLogin} />
-              <Route exact path='/register' render={renderRegister} />
-            </div>
-          </div>
+            <Route exact path='/login' render={renderLogin} />
+            <Route exact path='/register' render={renderRegister} />
+            <Route exact path='/' render={renderHome} />
           </div>
         </BrowserRouter>
-      </div>
+      </div >
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login';
+import Register from './Register'
+import './App.css';
+import {
+  Jumbotron,
+  Button,
+  Container,
+  Row,
+  Col,
+  NavbarBrand,
+  Navbar
+} from 'reactstrap';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <ul>
+            <li className="navbar">
+              Alibay
+              </li>
+            <li className="navbar">
+              <input type="text"></input>
+            </li>
+            <li className="navbar">
+              <input type="submit" />
+            </li>
+            <li className="navbar">
+              <Link to={"/login"}> Login </Link>
+            </li>
+            <li className="navbar">
+              <Link to={"/register"}> Register </Link>
+            </li>
+          </ul>
+          <div>
+            <Container>
+              <Row>
+                <Col xs={6} md={4}>
+                  <h4>Categories</h4>
+                  <ul>
+                    <li>Phones and Accessories</li>
+                    <li>Jewelry and Watches</li>
+                    <li>Consumer Electronics</li>
+                    <li>Cars</li>
+                  </ul>
+                </Col>
+                <Col xs={12} md={8}>
+                  <h1>Jumbotron</h1>
+                  <div>
+                    <Row>
+                      <Col>img1</Col>
+                      <Col>img2</Col>
+                      <Col>img3</Col>
+                    </Row>
+                    <Row>
+                      <Col>img4</Col>
+                      <Col>img5</Col>
+                      <Col>img6</Col>
+                    </Row>
+                    <Row>
+                      <Col>img7</Col>
+                      <Col>img8</Col>
+                      <Col>img9</Col>
+                    </Row>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
