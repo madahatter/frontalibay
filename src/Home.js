67f35e9ad10072f474ab9450cd7ce05b5432b9cd
madahatter <adamharper9@gmail.com>
2018-05-11 15:48:08 -0400

    homepage

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
 import { Register } from './Register';
 
 
+
+
 export default class Example extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      listings: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('/randomListings')
+      .then((res) => res.json())
+      .then((res) => this.setState({
+        listings: res.map((listing) => ({
+          src: '/' + listing.imageName,
+          altText: listing.title,
+          caption: '',
+          header: listing.title
+        }))
+      }))
+  }
+
+
   render() {
     return (
-        <div className="homeBody">
-          <div>
-            <img className="home-img" src="https://picsum.photos/700/400" />
-          </div>
+      <div className="homeBody">
+        <div>
+          <UncontrolledCarousel items={this.state.listings} />
         </div>
+      </div>
     );
   }
 }
