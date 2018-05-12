00bfb5c4b9f40fc328a62942bb3ef214c7d4dfe3
madahatter <adamharper9@gmail.com>
2018-05-12 13:25:11 -0400

    saturday

--- src/Confirmation.js
+++ src/Confirmation.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 class Confirmation extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        items: []
+      };
+    }
   render() {
     return (
       <div>
         <div>
           Confirmation Page
+          <div> test</div>
         </div>
       </div>
     );
