08782a70c96d0b12f83f4463b3a747ad3322d701
Daniyar Kasymjanov <kasymda@gmail.com>
2018-05-12 16:17:47 -0400

    dan

--- src/ListingCard.js
+++ src/ListingCard.js
@@ -25,22 +25,33 @@ class ListingCard extends React.Component {
   render() {
     return (
         <div>
-          <Card body outline color="secondary">
-            <img className="listingImg" src={'/' + this.props.imageName}/>
+          {/* <img src={'/' + this.props.imageName}/>
+           <div><Link to={'/itemDetails/' + this.props.itemID}>{this.props.title}</Link> <button onClick={this.handleClick}>Add to cart</button></div>
+           <div>{this.props.blurb}</div>
+           <div>{this.props.price}</div>
+           <div>{this.props.sellerID}</div>
+           <div>{this.props.category}</div> */}
+
+          <Card>
+            <div className="listingMainDiv">
+            <div className="imgDiv">
+              <img className="listingImg" src={'/' + this.props.imageName}/>
+            </div>
+            </div>
             <CardBody>
               <CardTitle><Link to={'/itemDetails/' + this.props.itemID}>{this.props.title}</Link></CardTitle>
               <CardText>Description: {this.props.blurb}</CardText>
               <CardText>Price: {this.props.price}$</CardText>
               <CardText>SellerID: {this.props.sellerID}</CardText>
               <CardText>Category: {this.props.category}</CardText>
-              <Button outline color="primary">
-                <Link to={"/itemsbySeller/" + this.props.sellerID}>Seller's info</Link>
-              </Button>
-              {this.props.buyerPage === true ? null :
-                <Button outline color="primary" onClick={this.handleClick}>
-                  Add to cart
-                </Button>
-              }
+              {this.props.buyerPage === true ?
+              null
+            :
+            <Button onClick={this.handleClick}>
+                Add to cart
+            </Button>
+            }
+              
             </CardBody>
           </Card>
 
